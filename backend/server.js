a443a43ee4f67977024d4686c6d9d6bd1ef89172
Rajashekar <gundarajashekarreddy@gmail.com>
2025-09-22 08:21:34 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -50,13 +50,6 @@ app.get("/",(req,res)=>{
 
 
 
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
 
 
 app.listen(PORT,()=> console.log(`server running on port ${PORT}`));
